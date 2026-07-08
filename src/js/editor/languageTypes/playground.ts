8127bce56f274461f2e5a5aab842329e5fa7a5fa
Dillon Wilson <greasyRooster1@gmail.com>
2026-07-08 09:56:10 -0400

    get input helper

--- src/js/editor/languageTypes/playground.ts
+++ src/js/editor/languageTypes/playground.ts
@@ -66,6 +66,20 @@ class PlaygroundType extends ProjectType {
         return "overload the getPlaygroundContent function in playground.ts to add your own content!";
     }
 
+    getInput(key:string):any{
+        let el = document.querySelector(`.playground-input #${key}`)
+        let type = el?.tagName.toLowerCase();
+        if(type=="input"){
+            return (el as HTMLInputElement).value;
+        }else if(type=="select"){
+            return (el as HTMLSelectElement).value;
+        }else if(type=="textarea"){
+            return (el as HTMLTextAreaElement).value;
+        }else {
+            return null;
+        }
+    }
+
     static getProjectDBData(projectName: string, lessonId: string):Promise<Object> {
         let cleanLessonId = lessonId ?? "none"
         return new Promise((resolve, reject) => {
