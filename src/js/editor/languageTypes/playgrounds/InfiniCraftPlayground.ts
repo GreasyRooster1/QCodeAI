0969db3eb565d1bb02992d8ba4e6c4888b3e96fb
GreasyRooster <greasyRooster1@gmail.com>
2026-07-11 16:46:21 -0700

    infini craft game

--- src/js/editor/languageTypes/playgrounds/InfiniCraftPlayground.ts
+++ src/js/editor/languageTypes/playgrounds/InfiniCraftPlayground.ts
@@ -115,13 +115,203 @@ class InfiniCraftPlayground extends PlaygroundType{
 
 const INFINI_RUNTIME_CODE = `
 
-function setup(){
+class Element{
+  constructor(x,y,type){
+    this.x=x;
+    this.y=y;
+    this.type=type;
+    textSize(20);
+    this.w = textWidth(this.type)+14;;
+    this.h = 30;
+    this.id=random(-99999999,9999999);
+    this.dead = false;
+  }
 
+  update(){
+    this.checkDrag();
+    this.draw();
+  }
+
+  draw(){
+    textSize(20);
+    
+    fill(7, 0, 15);
+    stroke(106, 0, 227);
+    strokeWeight(1);
+    rect(this.x,this.y,this.w,this.h,5);
+    
+    fill(255);
+    text(this.type,this.x+7,this.y+6);
+    textAlign(LEFT,TOP);
+  }
+  drawBorder(){
+    noFill();
+    stroke(106, 0, 227);
+    strokeWeight(sin(frameCount/25)*2+3);
+    rect(this.x,this.y,this.w,this.h,5);
+  }
+
+  checkDrag(){
+    if(!selectedEl) return;
+    
+    if(selectedEl.id==this.id){
+      this.x=mouseX-this.w/2;
+      this.y=mouseY-this.h/2;
+
+      for(let el of elements){
+        if(el.id==this.id)continue;
+        if(collision(el.x,el.y,el.w,el.h,this.x,this.y,this.w,this.h)){
+          this.drawBorder();
+          el.drawBorder();
+        }
+      }
+    }
+  }
+
+  combine(){
+    if(this.dead||this.id!=selectedEl.id)return;
+    let snap = [...elements];
+    for(let el of snap){
+      if(el.id==this.id) continue;
+      if(collision(el.x,el.y,el.w,el.h,this.x,this.y,this.w,this.h)){
+        this.dead=true;
+        el.dead=true;
+        let type = getType(this.type,el.type);
+        elements.push(new Element(el.x,el.y,type));
+      }
+    }
+  }
 }
 
-function draw(){
 
+const lineCount = 20;
+let elements = [];
+let discoveredElements = ["Air","Earth","Fire","Water"];
+let selectedEl = null;
+let mouseClicked = false;
+let lastMousePressed = false;
+let barOffset = 0;
+
+function setup() {
 }
+
+function draw() {
+  mouseClicked = mouseIsPressed&&!lastMousePressed;
+  
+  background(13, 0, 28);
+  drawBackground();
+  updateElements();
+  drawBar();
+  elements = elements.filter((e)=>!e.dead);
+  
+  lastMousePressed = mouseIsPressed;
+}
+
+function drawBar(){
+  fill(13, 0, 28);
+  rect(0,350,500,200);
+  let cols = segmentDiscovered();
+  drawDiscovered(cols);
+}
+
+function segmentDiscovered(){
+  let cols = [];
+  let cCol = -1;
+  for(let i=0;i<discoveredElements.length;i++){
+    if(i%4==0){
+      cCol++;
+      cols.push([]);
+    }
+    cols[cCol].push(discoveredElements[i]);
+  }
+  return cols;
+}
+
+function drawDiscovered(cols){
+  let runningW = 10;
+  let maxColW = 0;
+  for(let i=0;i<cols.length;i++){
+    for(let j=0;j<cols[i].length;j++){
+      let y = 358+j*35;
+      let w = drawEl(runningW+barOffset,y,cols[i][j]);
+      if(collision(runningW+barOffset,y,w,30,mouseX,mouseY,1,1)&&mouseClicked){
+        elements.push(new Element(250+random(-50,50),150+random(-50,50),cols[i][j]));
+      }
+      if(w>maxColW){
+        maxColW=w;
+      }
+    }
+    runningW+=maxColW+5;
+    maxColW=0;
+  }
+}
+
+function drawBackground(){
+  let ratio = width/lineCount;
+  stroke(139, 38, 255);
+  strokeWeight(.2);
+  for(let i=0;i<lineCount;i++){
+    line(i*ratio,0,i*ratio,width);
+  }
+  for(let i=0;i<lineCount;i++){
+    line(0,i*ratio,width,i*ratio);
+  }
+}
+
+function drawEl(x,y,type){
+  textSize(20);
+  let w = textWidth(type)+14;
+  
+  fill(7, 0, 15);
+  stroke(106, 0, 227);
+  strokeWeight(1);
+  rect(x,y,w,30,5);
+  
+  fill(255);
+  text(type,x+7,y+6);
+  textAlign(LEFT,TOP);
+
+  return w;
+}
+
+function updateElements(){
+  for(let el of elements){
+    el.update();
+  }
+}
+
+function mousePressed(){
+  for(let el of elements){
+    if(collision(el.x,el.y,el.w,el.h,mouseX,mouseY,1,1)){
+      selectedEl = el;
+    }
+  }
+}
+
+function mouseReleased(){
+  selectedEl.combine();
+  selectedEl = null;
+}
+
+function getType(){
+  return "WIP";
+}
+function mouseWheel(event){
+  barOffset+=event.delta/10;
+  barOffset = min(barOffset,0);
+}
+
+function collision(x1, y1, w1, h1, x2, y2, w2, h2) {
+  return (
+    x1 < x2 + w2 &&
+    x1 + w1 > x2 &&
+    y1 < y2 + h2 &&
+    y1 + h1 > y2
+  );
+}
+
+
+
 
 `
 
