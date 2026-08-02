16a754e0477c4b50dc3410310388d0bc44c6b71a
GreasyRooster <greasyRooster1@gmail.com>
2026-08-01 21:32:53 -0700

    fix starter project

--- src/js/login/data.js
+++ src/js/login/data.js
@@ -4,6 +4,7 @@ const requiredUserData = [
                 name:"Intro to AI",
                 timestamp:Date.now()/1000,
                 language:"llmsimple",
+                lessonId:"none",
             }
         }},
     {name:"badges",val:{0:{id:"user"}}},
